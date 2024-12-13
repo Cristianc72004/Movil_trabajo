41f22241b1423bfdd8e655e5366264c51864641f
Cristianc72004 <benalcazarc7@gmail.com>
2024-12-13 14:50:54 -0500

    composable

--- app/src/main/java/dev/practica/cristian2004/MainActivity.kt
+++ app/src/main/java/dev/practica/cristian2004/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.practica.cristian2004.ui.theme.CristianTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +20,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CristianTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Saludo()
         }
     }
 }
-
+//Funcion composable inicia con letra mayuscula y debe tener el
+// @composable antes de la funcion
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Saludo() {
+    Column(
+    modifier = Modifier.padding(vertical = 50.dp, horizontal = 16.dp))
+    {
+        Text(
+        text ="hola"
     )
-}
+    Text(
+        text ="Mi nombre es Cristian"
+    )
+}}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CristianTheme {
-        Greeting("Android")
-    }
-}
